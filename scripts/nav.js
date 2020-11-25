5622875b3120844e867119a91882b704cf993546
Aya-Watanabe <Aya-Watanabe@users.noreply.github.com>
2020-11-25 02:51:23 -0800

    question page changed

--- scripts/nav.js
+++ scripts/nav.js
@@ -4,6 +4,30 @@ const btn = document.getElementById('nav-button');
 
 btn.addEventListener('click', _ => nav.classList.toggle('show'));
 
-
 // prevent default
 btn.addEventListener('mousedown', (e) => { e.preventDefault() });
+
+
+//Acordion on FAQ page
+$(function() {
+    $('.question').click(function() {
+      var $answer = $(this).find('.answer');
+      //Check if answer is already open...
+      if($answer.hasClass('open')) { 
+        $answer.removeClass('open');
+        // Hide answer
+        $answer.slideUp();
+        // Change text from "-" to "+"
+        $(this).children('span').text('+');
+        
+      } else {
+        $answer.addClass('open'); 
+        //Show $answer
+         $answer.slideDown();
+        // Change text from "+" to "-"
+        $(this).children('span').text('－');
+        
+      }
+    });
+  });
+  
